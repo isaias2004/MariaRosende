6361922c5fdb7c54d440164f8c722a93d89737a6
Isaias <lawicki76@gmail.com>
2022-04-04 16:56:35 -0300

    Fix the error in HTTP

--- script.js
+++ script.js
@@ -5,14 +5,3 @@ $(".hambergur").on("click", () => {
 $(".close").on("click", () => {
   $(".fullscreen").toggleClass("reverse_anim");
 });
-
-
-window.onload = function(){
-  var formulario = document.getElementById('formulario');
-  var boton_enviar = document.getElementById('enviar');
-  boton_enviar.onclick = function(){
-     if(formulario.checkValidity()){
-        alert('El formulario es correcto!');
-     }
-  }
-}
