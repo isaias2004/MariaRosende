09bbb22a2bb66e47f9ffac73c0da57b87e8e256b
Isaias <lawicki76@gmail.com>
2022-07-29 14:22:31 -0300

    Add another changes in javascript

--- script.js
+++
@@ -1,8 +0,0 @@
-$(".hambergur").on("click", () => {
-  $(".fullscreen").toggleClass("active").removeClass("reverse_anim");
-});
-
-$(".close").on("click", () => {
-  $(".fullscreen").toggleClass("reverse_anim");
-});
-
